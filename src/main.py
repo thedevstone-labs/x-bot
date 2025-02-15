57dffcf92ea28be47bf08098600512e36c84e28f
Luca <luca.giulianini2@studio.unibo.it>
2025-02-15 12:01:33 +0100

    refactor

--- src/main.py
+++ src/main.py
@@ -11,13 +11,13 @@ from utils import get_path, init_logger, load_config
 
 async def main():
     init_logger()
-    self__config = load_config()
+    config = load_config()
     tweet_path: Path = get_path("./tweets.csv")
     # Scraping
-    scraper: Scraper = Scraper(self__config.auth, skip_login=True)
+    scraper: Scraper = Scraper(config.auth, skip_login=True)
     await scraper.login()
-    tweets: List[Tweet] = await scraper.get_tweets(self__config.scraper.query,
-                                                   self__config.scraper.tweet_number)
+    tweets: List[Tweet] = await scraper.get_tweets(config.scraper.query,
+                                                   config.scraper.tweet_number)
 
     scraper.save_tweets(tweets, tweet_path)
     # Data Analysis
